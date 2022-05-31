9de16d24cf4a0d18a555efe41ee10140cebe5466
wulinfeng <15395401361@163.com>
2022-05-31 11:59:19 +0800

    init framework

---
+++ Frameworks/GFSDK.framework/Headers/LoginResponse.h
@@ -0,0 +1,16 @@
+//
+//  LoginResponse.h
+//  GFSDK
+//
+//  Created by Zhu on 2021/12/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginResponse : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
